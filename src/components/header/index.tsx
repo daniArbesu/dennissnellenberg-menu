00064909e55dc9661cf2e5bfe53377457cdc2c7d
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-07-23 14:10:14 +0200

    feat: basis project structure

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useState } from 'react';
+import styles from './style.module.css';
+
+const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return <div>Header</div>;
+};
+
+export default Header;
